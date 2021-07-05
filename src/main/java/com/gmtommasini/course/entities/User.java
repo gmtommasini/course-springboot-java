01e4293e16616e3b89ffad44cea26c181ffe6f7b
Guilherme Tommasini <gmtommasini@gmail.com>
2021-07-05 11:01:42 -0400

    H2 database, test profile User jpa

--- src/main/java/com/gmtommasini/course/entities/User.java
+++ src/main/java/com/gmtommasini/course/entities/User.java
@@ -2,10 +2,17 @@ package com.gmtommasini.course.entities;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
-	
+import javax.persistence.Entity; //Hibernate is the implementation of javax.persistence
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable{	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
